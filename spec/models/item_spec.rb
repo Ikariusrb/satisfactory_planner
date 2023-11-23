69af265aabefc390297b4cb28bdd5ae479cb034b
Ross Becker <ross.becker@gmail.com>
2023-11-23 11:27:12 -0800

    run migration

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  unique_items  (name) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
