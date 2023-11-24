62b5de06f78772114280617cab8e79ab4ac65682
Ross Becker <ross.becker@gmail.com>
2023-11-24 14:24:39 -0800

    run db migration

--- app/models/production_stage.rb
+++ app/models/production_stage.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: production_stages
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  location_id :bigint
+#  plan_id     :bigint           not null
+#
+# Indexes
+#
+#  index_production_stages_on_location_id       (location_id)
+#  index_production_stages_on_plan_id           (plan_id)
+#  index_production_stages_on_plan_id_and_name  (plan_id,name) UNIQUE
+#
 class ProductionStage < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_24_171833) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_24_220159) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,17 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_171833) do
     t.index ["admin_user_id", "name"], name: "index_plans_on_admin_user_id_and_name", unique: true
     t.index ["admin_user_id"], name: "index_plans_on_admin_user_id"
     t.index ["name"], name: "unique_plans", unique: true
+  end
+
+  create_table "production_stages", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "plan_id", null: false
+    t.bigint "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_production_stages_on_location_id"
+    t.index ["plan_id", "name"], name: "index_production_stages_on_plan_id_and_name", unique: true
+    t.index ["plan_id"], name: "index_production_stages_on_plan_id"
   end
 
 end
