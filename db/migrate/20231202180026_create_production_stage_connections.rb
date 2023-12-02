ad26faf087a544b9eec510f657f29f31ca499a5d
Ross Becker <ross.becker@gmail.com>
2023-12-02 10:18:49 -0800

    fix

--- db/migrate/20231202180026_create_production_stage_connections.rb
+++ db/migrate/20231202180026_create_production_stage_connections.rb
@@ -1,8 +1,8 @@
 class CreateProductionStageConnections < ActiveRecord::Migration[7.1]
   def change
     create_table :production_stage_connections do |t|
-      t.references :input, index: true, foreign_key: { to_table: :production_stage_ouputs }
-      t.references :output, index: true, foreign_key: { to_table: :production_stage_inputs }
+      t.references :source, index: true
+      t.references :destination, index: true
 
       t.timestamps
     end
