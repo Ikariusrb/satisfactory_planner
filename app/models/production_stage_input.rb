00e1009d88873b7bd4df46df4ff8d802a6a93f0e
Ross Becker <ross.becker@gmail.com>
2023-12-02 10:01:11 -0800

    run migration

--- app/models/production_stage_input.rb
+++ app/models/production_stage_input.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: production_stage_inputs
+#
+#  id                  :bigint           not null, primary key
+#  rate                :decimal(9, 3)    not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  item_id             :bigint           not null
+#  production_stage_id :bigint           not null
+#
+# Indexes
+#
+#  index_production_stage_inputs_on_item_id              (item_id)
+#  index_production_stage_inputs_on_production_stage_id  (production_stage_id)
+#
 class ProductionStageInput < ApplicationRecord
 end
--- spec/models/production_stage_output_spec.rb
+++ spec/models/production_stage_output_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: production_stage_outputs
+#
+#  id                  :bigint           not null, primary key
+#  rate                :decimal(9, 3)    not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  item_id             :bigint           not null
+#  production_stage_id :bigint           not null
+#
+# Indexes
+#
+#  index_production_stage_outputs_on_item_id              (item_id)
+#  index_production_stage_outputs_on_production_stage_id  (production_stage_id)
+#
 require 'rails_helper'
 
 RSpec.describe ProductionStageOutput, type: :model do
